c2fa26b238e1933b6cc426c3fcea531940cfb3c8
Supreeth Ragavendra s <supreethragavendra.s@aximsoft.com>
2026-06-14 20:45:51 +0530

    Update Task6.js

--- JS-problems/Task6.js
+++ JS-problems/Task6.js
@@ -3,27 +3,43 @@
 
 // Example:
 // "Listen" and "Silent" → true
-
 function getStringLength(str) {
   let count = 0;
 
   for (let char of str) {
     count++;
   }
+
   return count;
 }
 
 function Anagram(str1, str2) {
   let n1 = getStringLength(str1);
   let n2 = getStringLength(str2);
-
-  if (n1 != n2) {
+  if (n1 !== n2) {
     return false;
-  } else {
   }
+
+  let used = [];
+
+  for (let i = 0; i < n1; i++) {
+    let found = false;
+
+    for (let j = 0; j < n2; j++) {
+      if (str1[i] === str2[j] && !used[j]) {
+        used[j] = true;
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-let str1 = "Listen";
-let str2 = "Silent";
-
-console.log(Anagram(str1, str2));
+console.log(Anagram("listen", "silent")); // true
+console.log(Anagram("hello", "world")); // false
